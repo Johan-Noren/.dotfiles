73f93d7ae62ada3f9bf59dc1406810533b5b678d
Johan Norén <71132454+Johan-Noren@users.noreply.github.com>
2021-03-08 13:54:35 +0100

    Update swaybar_helper.sh

--- .local/bin/swaybar_helper.sh
+++ .local/bin/swaybar_helper.sh
@@ -27,8 +27,8 @@ while :
 do
     TIME="$(date +'%H:%M')"
     BATTERY_CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
-    WIFI_SSID=$(iw dev wlp3s0  link | awk '/SSID/{print $2}')
-    WIFI_DB=$(iw dev wlp3s0 link | grep signal | cut -d' ' -f 2 | cut -d'-' -f 2)
+    WIFI_SSID=$(iw dev wlan0  link | awk '/SSID/{print $2}')
+    WIFI_DB=$(iw dev wlan0 link | grep signal | cut -d' ' -f 2 | cut -d'-' -f 2)
 
     # ECHO OUT THE STATUSLINE
     echo "$(wifi_bar)$_AND_$(battery_bar)%$_AND_$(time_bar) "
