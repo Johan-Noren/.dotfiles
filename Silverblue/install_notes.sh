6ff2ba074d4beb8071bd8bd2dd9fc52e9f8beb52
Johan Norén <71132454+Johan-Noren@users.noreply.github.com>
2021-03-22 09:45:11 +0100

    Update and rename install_notes.md to install_notes.sh

---
+++ Silverblue/install_notes.sh
@@ -0,0 +1,23 @@
+# Run after install
+
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.gnome.mutter center-new-windows 'true'
+gsettings set org.gnome.mutter dynamic-workspaces False
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+
+
+# Remove these keybindings. cus' they sux
+gsettings set org.gnome.shell.keybindings switch-to-application-1 [] 
+gsettings set org.gnome.shell.keybindings switch-to-application-2 []  
+gsettings set org.gnome.shell.keybindings switch-to-application-3 []  
+gsettings set org.gnome.shell.keybindings switch-to-application-4 []  
+gsettings set org.gnome.shell.keybindings switch-to-application-5 []  
+gsettings set org.gnome.shell.keybindings switch-to-application-6 []  
+gsettings set org.gnome.shell.keybindings switch-to-application-7 []  
+gsettings set org.gnome.shell.keybindings switch-to-application-8 []  
+gsettings set org.gnome.shell.keybindings switch-to-application-9 []  
+
+# Create toolbox with optional name
+toolbox create <name>
+dnf install zsh
